fc2752d9c68c08dc86cded408abd008f6388ab46
chaeun99 <hyc9328@naver.com>
2025-12-11 23:22:24 +0900

    [refactor] Reply 엔티티의 @Data를 @Getter로 교체

--- src/main/java/com/example/board_project/entity/Reply.java
+++ src/main/java/com/example/board_project/entity/Reply.java
@@ -2,6 +2,7 @@ package com.example.board_project.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -9,7 +10,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 import java.time.LocalDateTime;
 
 @Entity
-@Data
+@Getter
 @NoArgsConstructor
 @Table(name = "reply")
 @EntityListeners(AutoCloseable.class)
